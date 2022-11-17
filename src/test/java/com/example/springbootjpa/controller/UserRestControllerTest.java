30a81769b5f1838fd055ffe1bafd049d6f8c8bbe
이소영 <th42500@naver.com>
2022-11-17 15:29:25 +0900

    [Feat] UserRestControllerTest - 사용자 등록 기능 테스트
    java.lang.AssertionError: No value at JSON path "$.id" 에러 발생

--- src/test/java/com/example/springbootjpa/controller/UserRestControllerTest.java
+++ src/test/java/com/example/springbootjpa/controller/UserRestControllerTest.java
@@ -1,17 +1,23 @@
 package com.example.springbootjpa.controller;
 
+import com.example.springbootjpa.domain.dto.UserRequestDto;
 import com.example.springbootjpa.domain.dto.UserResponseDto;
 import com.example.springbootjpa.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,6 +27,8 @@ class UserRestControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     UserService userService;
@@ -40,5 +48,29 @@ class UserRestControllerTest {
                 .andDo(print());
 
         verify(userService).getUser(1L);
+    }
+
+    @Test
+    @DisplayName("addUser 테스트")
+    void addUserTest() throws Exception {
+        UserRequestDto dto = UserRequestDto.builder()
+                .username("testName")
+                .password("testPassword")
+                .build();
+
+        given(userService.insertUser(dto))
+                .willReturn(new UserResponseDto(dto.getUsername(), "회원 등록 성공"));
+
+
+        mockMvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(dto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").exists())
+                .andExpect(jsonPath("$.username").exists())
+                .andExpect(jsonPath("$.message").exists())
+                .andDo(print());
+
+        verify(userService.insertUser(ArgumentMatchers.refEq(dto)));
     }
 }
